9e9c8a10e5ad8271eac2db7d20a503520fb2c257
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-06-21 19:45:26 -0500

    Taller 20 - Uso de APIs

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <!--<img alt="Vue logo" src="./assets/logo.png" />
   <Contador titulo="Estudiante" v-bind:inicio="105" />
-  <Contador titulo="Profesor" :inicio="101"/>
+  <Contador titulo="Profesor" :inicio="101"/>-->
+  <CaracolaMagica/>
 </template>
 <script>
-import Contador from "./components/Contador.vue";
+//import Contador from "./components/Contador.vue";
+import CaracolaMagica from "./components/CaracolaMagica.vue";
 
 export default {
   name: "App",
   components: {
-    Contador,
+    CaracolaMagica,
   },
 };
 </script>
---
+++ src/components/CaracolaMagica.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Caracola Mágica</h1>
+  <img
+    src="https://play-lh.googleusercontent.com/Y3eEPFBdq4wv2x8XPg9hP06QIvc3tW7Mo8kba4H4OyQ4QqhFOo_43hwT-0-Mp9WHQ6I=w240-h480-rw"
+    alt="No se puede presentar la imagen"
+  /><br />
+  <input type="text" placeholder="Hazme una pregunta" v-model="pregunta" />
+  <p>Recuerda Terminar con un enter la pregunta</p>
+  <div>
+    <h2>Voy a pasar de año?</h2>
+    <h1>SI,NO</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      pregunta: "Hola Mundo",
+    };
+  },
+};
+</script>
+
+<style></style>
