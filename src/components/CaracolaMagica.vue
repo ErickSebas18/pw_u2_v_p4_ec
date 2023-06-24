16614e82bfc0bdd1f87ea70032b2929a111fe422
Erick Chávez <ericksebastianchavezgarcia4gmail.com>
2023-06-23 23:40:31 -0500

    Hacer juego API

--- src/components/CaracolaMagica.vue
+++ src/components/CaracolaMagica.vue
@@ -7,8 +7,13 @@
   /><br />
   <div class="bg-dark"></div>
   <div class="contenedor">
+    <div class="mensaje" v-if="mensaje">
+        <h1 v-if="mensaje">{{ mensaje }}</h1>
+        <h1 v-if="cont">{{ cont }}</h1>
+      </div>
     <input type="text" placeholder="Hazme una pregunta" v-model="pregunta" />
     <p>Recuerda Terminar con el signo "?"</p>
+    <p>Acumula "yes" y suma puntos</p>
     <div>
       <h2>{{ pregunta }}</h2>
       <h1>{{ respuesta }}</h1>
@@ -20,9 +25,12 @@
 export default {
   data() {
     return {
+      indice: 1,
       pregunta: "",
       respuesta: "",
       imagen: null,
+      mensaje: null,
+      cont: "",
     };
   },
   watch: {
@@ -43,13 +51,25 @@ export default {
       const { answer, image } = respuesta;
       this.respuesta = answer;
       this.imagen = image;
+      this.verificar();
+    },
+
+    verificar() {
+      if (this.respuesta.toLowerCase() === "yes") {
+        this.mensaje = "GANASTE";
+        this.cont = "Ganaste: " + this.indice;
+        this.indice++;
+      } else {
+        this.mensaje = "PERDISTE";
+      }
     },
   },
 };
 </script>
 
 <style>
-img, .bg-dark {
+img,
+.bg-dark {
   height: 100vh;
   width: 100vw;
   left: 0px;
@@ -59,26 +79,38 @@ img, .bg-dark {
   top: 0px;
 }
 
-.bg-dark{
+.bg-dark {
   background-color: rgba(0, 0, 0, 0.4);
 }
-.contenedor{
+.contenedor {
   position: relative;
 }
 
-input{
+input {
   width: 250px;
   padding: 10px 15px;
   border-radius: 5px;
   border: none;
 }
 
-p, h1, h2 {
+p,
+h1,
+h2 {
   color: white;
 }
 
 p {
   font-size: 25px;
   margin-top: 0px;
+}
+
+.mensaje{
+  border: solid black;
+  width: 200px;
+  padding: 15px;
+  border-radius: 15px;
+  margin: auto;
+  margin-bottom: 10px;
+  box-shadow: 0 0 10px 5px rgba(252, 245, 188, 0.4);
 }
 </style>
